3cd6dd400dfcf84a43819a256e7accb910c676c2
abhishek002002 <63637355+abhishek002002@users.noreply.github.com>
2021-02-11 17:45:29 +0530

    Delete modules.js

--- modules.js
+++
@@ -1,8 +0,0 @@
-const xyz=  require('./people');
-
-console.log(xyz);
-
-// const os = require('os');
-// const { captureRejectionSymbol } = require('events');
-
-// console.log(os.platform(), os.homedir());
